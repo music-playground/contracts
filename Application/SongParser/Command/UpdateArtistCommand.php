74ee448dcc53ed85e9f434ae0ed0f3512e398418
WhtsPoint <whtspoint@gmail.com>
2025-02-08 22:59:19 +0200

    Another namespace fix

--- Application/SongParser/Command/UpdateArtistCommand.php
+++ Application/SongParser/Command/UpdateArtistCommand.php
@@ -1,8 +1,9 @@
 <?php
 
-namespace MusicPlayground\Contract\Application\SongParser\DTO;
+namespace MusicPlayground\Contract\Application\SongParser\Command;
 
 use MusicPlayground\Contract\Application\Command\CommandWithIdInterface;
+use MusicPlayground\Contract\Application\SongParser\DTO\ArtistDTO;
 
 final readonly class UpdateArtistCommand implements CommandWithIdInterface
 {
