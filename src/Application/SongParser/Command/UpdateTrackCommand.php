38f7ae8aad612447afa66f65265cf7655de42e81
WhtsPoint <whtspoint@gmail.com>
2025-02-19 20:19:19 +0200

    Fix

--- src/Application/SongParser/Command/UpdateTrackCommand.php
+++ src/Application/SongParser/Command/UpdateTrackCommand.php
@@ -2,11 +2,17 @@
 
 namespace MusicPlayground\Contract\Application\SongParser\Command;
 
+use MusicPlayground\Contract\Application\Command\CommandWithIdInterface;
 use MusicPlayground\Contract\Application\SongParser\DTO\TrackDTO;
 
-final readonly class UpdateTrackCommand
+final readonly class UpdateTrackCommand implements CommandWithIdInterface
 {
     public function __construct(public TrackDTO $dto)
     {
+    }
+
+    public function getId(): string
+    {
+        return $this->dto->source->id;
     }
 }
