6a0e3554770c72668da5e533db948821953b3877
WhtsPoint <whtspoint@gmail.com>
2025-03-06 14:40:25 +0200

    AlbumDTO add artistId

--- src/Application/SongParser/Command/UpdateAlbumCommand.php
+++ src/Application/SongParser/Command/UpdateAlbumCommand.php
@@ -7,8 +7,10 @@ use MusicPlayground\Contract\Application\SongParser\DTO\AlbumDTO;
 
 final readonly class UpdateAlbumCommand implements CommandWithIdInterface
 {
-    public function __construct(public AlbumDTO $dto)
-    {
+    public function __construct(
+        public string $artistId,
+        public AlbumDTO $dto
+    ) {
     }
 
     public function getId(): string
