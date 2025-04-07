0bbc04926f04e2201ea1f4e1e551a838a72495b0
WhtsPoint <whtspoint@gmail.com>
2025-04-08 00:09:57 +0300

    OnUpdateAllCommand update

--- src/Application/SongParser/Command/OnUpdateAlbumCommand.php
+++ src/Application/SongParser/Command/OnUpdateAlbumCommand.php
@@ -4,13 +4,11 @@ namespace MusicPlayground\Contract\Application\SongParser\Command;
 
 use MusicPlayground\Contract\Application\Command\CommandWithIdInterface;
 use MusicPlayground\Contract\Application\SongParser\DTO\AlbumSourceDTO;
-use MusicPlayground\Contract\Application\SongParser\DTO\ArtistSourceDTO;
 
 final readonly class OnUpdateAlbumCommand implements CommandWithIdInterface
 {
     public function __construct(
         public AlbumSourceDTO $source,
-        public ArtistSourceDTO $artistSource,
         /** @var array<string> */
         public array $containsTracks
     ) {
