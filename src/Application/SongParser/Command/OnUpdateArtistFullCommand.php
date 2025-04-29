2190aa680aaaf0ee2835729e42c1618365200062
WhtsPoint <whtspoint@gmail.com>
2025-04-29 17:03:35 +0300

    OnUpdateArtistFullCommand update

--- src/Application/SongParser/Command/OnUpdateArtistFullCommand.php
+++ src/Application/SongParser/Command/OnUpdateArtistFullCommand.php
@@ -2,13 +2,19 @@
 
 namespace MusicPlayground\Contract\Application\SongParser\Command;
 
+use MusicPlayground\Contract\Application\Command\CommandWithIdInterface;
 use MusicPlayground\Contract\Application\SongParser\DTO\ArtistDTO;
 
-final readonly class OnUpdateArtistFullCommand
+final readonly class OnUpdateArtistFullCommand implements CommandWithIdInterface
 {
     public function __construct(
         public ArtistDTO $artist,
         public array $containsAlbums
     ) {
+    }
+
+    public function getId(): string
+    {
+        return $this->artist->source->id;
     }
 }
