f22f54cf2e6d88bf9340a0515afe10b6fb5726b8
WhtsPoint <whtspoint@gmail.com>
2025-04-29 15:13:44 +0300

    OnUpdateArtistFullCommand creation

---
+++ src/Application/SongParser/Command/OnUpdateArtistFullCommand.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace MusicPlayground\Contract\Application\SongParser\Command;
+
+use MusicPlayground\Contract\Application\SongParser\DTO\ArtistDTO;
+
+final readonly class OnUpdateArtistFullCommand
+{
+    public function __construct(public ArtistDTO $artist) {
+    }
+}
