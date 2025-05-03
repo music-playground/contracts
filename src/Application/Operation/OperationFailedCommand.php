204015d1a2dd08d1215b864e0c15572fb820672d
WhtsPoint <whtspoint@gmail.com>
2025-05-03 23:25:03 +0300

    OperationFailedCommand creation

---
+++ src/Application/Operation/OperationFailedCommand.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace MusicPlayground\Contract\Application\Operation;
+
+use InvalidArgumentException;
+
+final readonly class OperationFailedCommand
+{
+    /** @var string|string[] */
+    public string|array $errors;
+
+    public function __construct(
+        public string $operationId,
+        string|array $errors
+    ) {
+        if (is_array($errors) === true) {
+            if (count($errors) === 0) {
+                throw new InvalidArgumentException('Empty errors array');
+            }
+
+            $this->errors = count($errors) === 1 ? $errors[0] : $errors;
+        } else {
+            $this->errors = $errors;
+        }
+    }
+}
