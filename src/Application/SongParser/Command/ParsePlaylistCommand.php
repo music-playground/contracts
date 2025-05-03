2f97cb7542020aaf9e284e9e892a26c6e7b5f555
WhtsPoint <whtspoint@gmail.com>
2025-05-03 22:09:04 +0300

    ParsePlaylistCommand update

--- src/Application/SongParser/Command/ParsePlaylistCommand.php
+++ src/Application/SongParser/Command/ParsePlaylistCommand.php
@@ -3,12 +3,16 @@
 namespace MusicPlayground\Contract\Application\SongParser\Command;
 
 use MusicPlayground\Contract\Application\Command\CommandWithIdInterface;
+use Symfony\Component\Uid\UuidV4;
 
 final readonly class ParsePlaylistCommand implements CommandWithIdInterface
 {
+    public string $operationId;
+
     public function __construct(
         public string $playlistId
     ) {
+        $this->operationId = UuidV4::v4()->toString();
     }
 
     public function getId(): string
