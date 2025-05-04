7ff1b299f05e6edc6c7bf6efcf3979057a1603b3
WhtsPoint <whtspoint@gmail.com>
2025-05-05 01:27:38 +0300

    OnCreatedPlaylistCommand update

--- src/Application/Playlist/Command/OnCreatedPlaylistCommand.php
+++ src/Application/Playlist/Command/OnCreatedPlaylistCommand.php
@@ -2,13 +2,26 @@
 
 namespace MusicPlayground\Contract\Application\Playlist\Command;
 
+use MusicPlayground\Contract\Application\Command\CommandWithIdInterface;
+use MusicPlayground\Contract\Application\Command\OperationCommandInterface;
 use MusicPlayground\Contract\Application\Playlist\DTO\PlaylistSourceDTO;
 
-final readonly class OnCreatedPlaylistCommand
+final readonly class OnCreatedPlaylistCommand implements CommandWithIdInterface, OperationCommandInterface
 {
     public function __construct(
+        public string $operationId,
         public string $id,
         public PlaylistSourceDTO $source
     ) {
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function getOperationId(): string
+    {
+        return $this->operationId;
     }
 }
