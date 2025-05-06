fc0b84b0d12e98457e69d309c00f514fbae28876
WhtsPoint <whtspoint@gmail.com>
2025-05-06 15:24:27 +0300

    UpdateAlbumForPlaylistCommand update

--- src/Application/SongParser/Command/UpdateAlbumForPlaylistCommand.php
+++ src/Application/SongParser/Command/UpdateAlbumForPlaylistCommand.php
@@ -10,6 +10,7 @@ final readonly class UpdateAlbumForPlaylistCommand implements CommandWithIdInter
 {
     public function __construct(
         public string $operationId,
+        public string $playlistId,
         public AlbumDTO $album,
         public PartialTrackDTO $track
     ) {
